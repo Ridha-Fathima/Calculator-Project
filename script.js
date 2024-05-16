b59b0d9ff6b4ff8a79ad0f7c1eb836840b42ce1f
Ridha-Fathima <149472100+Ridha-Fathima@users.noreply.github.com>
2024-05-16 10:55:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function calculate(operator) {
+
+
+let inputElement1 = parseFloat(document.getElementById("display1").value);
+let inputElement2 = parseFloat(document.getElementById("display2").value);
+let resultElement;
+
+    switch(operator) {
+
+        case '+':
+            resultElement = inputElement1 + inputElement2;
+            break;
+
+         case '-':
+            resultElement = inputElement1 - inputElement2;
+            break;
+
+        case '*':
+            resultElement = inputElement1 * inputElement2;
+            break;
+
+        case '/':
+            resultElement = inputElement1 / inputElement2;
+            break;
+
+        default:
+            resultElement = "Invalid Operator";
+            
+
+    }
+
+    document.getElementById("result").innerHTML = "Result: " + resultElement;
+}
+
+function clearCalculate() {
+
+    document.getElementById("display1").value = "";
+    document.getElementById("display2").value = "";
+    document.getElementById("result").value = "";
+   
+}
+
+
+
+
+
+
+
+
